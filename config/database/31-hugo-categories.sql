37e9fd12ede274450d6279a8849193a3c3fdce8a
David Matthewman <david@matthewman.org>
2018-10-08 13:00:35 +0100

    Add a 'competition' field to all Hugo-related tables.

--- config/database/31-hugo-categories.sql
+++ config/database/31-hugo-categories.sql
@@ -3,3 +3,6 @@ SET ROLE hugo;
 CREATE TYPE Category AS ENUM ('Novel', 'Novella', 'Novelette', 'ShortStory', 'RelatedWork',
     'GraphicStory', 'DramaticLong', 'DramaticShort', 'EditorLong', 'EditorShort', 'ProArtist',
     'Semiprozine', 'Fanzine', 'Fancast', 'FanWriter', 'FanArtist', 'Series', 'NewWriter');
+    
+CREATE TYPE Competition AS ENUM ('Hugos', 'Retro Hugos');
+
