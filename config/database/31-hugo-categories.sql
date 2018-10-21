e29e04c833f2d228075079cd47bf1f33dbf59d54
David Matthewman <david@matthewman.org>
2018-10-21 17:08:06 +0100

    Added the Retro Hugo categories temporarily to the database.

--- config/database/31-hugo-categories.sql
+++ config/database/31-hugo-categories.sql
@@ -2,7 +2,11 @@ SET ROLE hugo;
 
 CREATE TYPE Category AS ENUM ('Novel', 'Novella', 'Novelette', 'ShortStory', 'RelatedWork',
     'GraphicStory', 'DramaticLong', 'DramaticShort', 'EditorLong', 'EditorShort', 'ProArtist',
-    'Semiprozine', 'Fanzine', 'Fancast', 'FanWriter', 'FanArtist', 'Series', 'NewWriter');
+    'Semiprozine', 'Fanzine', 'Fancast', 'FanWriter', 'FanArtist', 'Series', 'NewWriter', 'Lodestar',
+    'RetroNovel', 'RetroNovella', 'RetroNovelette', 'RetroShortStory', 'RetroRelatedWork', 'RetroGraphicStory',
+    'RetroDramaticLong', 'RetroDramaticShort', 'RetroEditorLong', 'RetroEditorShort', 'RetroProArtist', 'RetroSemiprozine',
+    'RetroFanzine', 'RetroFancast', 'RetroFanWriter', 'RetroFanArtist', 'RetroSeries');
+
     
 CREATE TYPE Competition AS ENUM ('Hugos', 'Retro Hugos');
 
