434318684dfdf6efe7afd47a9c5fe4779e4cffe8
David Matthewman <david@matthewman.org>
2019-04-07 14:35:33 +0100

    Add 'BestArtBook' and 'RetroBestArtBook' to the Hugo categories.

--- config/database/31-hugo-categories.sql
+++ config/database/31-hugo-categories.sql
@@ -2,10 +2,12 @@ SET ROLE hugo;
 
 CREATE TYPE Category AS ENUM ('Novel', 'Novella', 'Novelette', 'ShortStory', 'RelatedWork',
     'GraphicStory', 'DramaticLong', 'DramaticShort', 'EditorLong', 'EditorShort', 'ProArtist',
-    'Semiprozine', 'Fanzine', 'Fancast', 'FanWriter', 'FanArtist', 'Series', 'NewWriter', 'Lodestar',
+    'Semiprozine', 'Fanzine', 'Fancast', 'FanWriter', 'FanArtist', 'Series', 'NewWriter',
+    'BestArtBook', 'Lodestar',
     'RetroNovel', 'RetroNovella', 'RetroNovelette', 'RetroShortStory', 'RetroRelatedWork', 'RetroGraphicStory',
     'RetroDramaticLong', 'RetroDramaticShort', 'RetroEditorLong', 'RetroEditorShort', 'RetroProArtist', 'RetroSemiprozine',
-    'RetroFanzine', 'RetroFancast', 'RetroFanWriter', 'RetroFanArtist', 'RetroSeries');
+    'RetroFanzine', 'RetroFancast', 'RetroFanWriter', 'RetroFanArtist', 'RetroSeries', 
+    'RetroBestArtBook');
 
     
 CREATE TYPE Competition AS ENUM ('Hugos', 'Retro Hugos');
