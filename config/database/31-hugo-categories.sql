19d6f984a7962507a222355fa8413bc6ae23ec96
David Matthewman <david@matthewman.org>
2021-02-13 12:17:25 +0000

    Add Best Video Game as a plausible category

--- config/database/31-hugo-categories.sql
+++ config/database/31-hugo-categories.sql
@@ -3,7 +3,7 @@ SET ROLE hugo;
 CREATE TYPE Category AS ENUM ('Novel', 'Novella', 'Novelette', 'ShortStory', 'RelatedWork',
     'GraphicStory', 'DramaticLong', 'DramaticShort', 'EditorLong', 'EditorShort', 'ProArtist',
     'Semiprozine', 'Fanzine', 'Fancast', 'FanWriter', 'FanArtist', 'Series', 'NewWriter',
-    'BestArtBook', 'Lodestar',
+    'BestArtBook', 'BestVideoGame', 'Lodestar',
     'RetroNovel', 'RetroNovella', 'RetroNovelette', 'RetroShortStory', 'RetroRelatedWork', 'RetroGraphicStory',
     'RetroDramaticLong', 'RetroDramaticShort', 'RetroEditorLong', 'RetroEditorShort', 'RetroProArtist', 'RetroSemiprozine',
     'RetroFanzine', 'RetroFancast', 'RetroFanWriter', 'RetroFanArtist', 'RetroSeries', 
